0ede2d1b1617ba3b4e073a24c7a2a1e96c2217ee
korrio <korr@aq1.co>
2020-12-06 12:18:52 +0700

    init for Sakda

---
+++ server.js
@@ -0,0 +1,29 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+io.on('connection', client => {
+
+  console.info(`Client connected [id=${client.id}]`);
+  // initialize this client's sequence number
+  sequenceNumberByClient.set(client, 1);
+
+  // when socket disconnects, remove it from the list:
+  client.on("disconnect", () => {
+    sequenceNumberByClient.delete(client);
+    console.info(`Client gone [id=${client.id}]`);
+  });
+
+  client.on('event', data => { /* … */ });
+  //client.on('disconnect', () => { /* … */ });
+});
+server.listen(3000);
+
+let
+  sequenceNumberByClient = new Map();
+
+// sends each client its current sequence number
+setInterval(() => {
+  for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
+    client.emit("seq-num", sequenceNumber);
+    sequenceNumberByClient.set(client, sequenceNumber + 1);
+  }
+}, 1000);
